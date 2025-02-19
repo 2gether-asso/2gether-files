c55e6609ba96c3926eda634486ccd3c02125cbf1
Matiboux <matiboux@gmail.com>
2025-02-19 22:21:50 +0100

    Update packages & build config

--- app/app/astro.config.mjs
+++ app/app/astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config'
-
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://files.2gether-asso.fr',
-	integrations: [
-		tailwind(),
-	],
-});
+	vite: {
+		plugins: [
+			tailwindcss(),
+		],
+	},
+})
