adbab6b5617c38a19f17baac7e5840efe99cc41e
Matiboux <matiboux@gmail.com>
2025-02-24 02:14:40 +0100

    Update layouts & site config

--- app/app/src/site.ts
+++ app/app/src/site.ts
@@ -1,20 +1,31 @@
-import type { I18nKeys } from '~/i18n/type.d.ts'
+import type { Props as BaseProps } from '~/layouts/Base.astro'
 
 export interface Site
 {
-	title?: string
-	description?: string | I18nKeys
-	author?: string
-	keywords?: string[]
-	themeColor?: string
-	favicon?: string
-	lang?: string
+	lang?: BaseProps['lang']
+	title?: BaseProps['title']
+	description?: BaseProps['description']
+	author?: BaseProps['author']
+	keywords?: BaseProps['keywords']
+	generator?: BaseProps['generator']
+	themeColor?: BaseProps['themeColor']
+	viewportScale?: BaseProps['viewportScale']
+	favicon?: BaseProps['favicon']
+	socialTitle?: BaseProps['socialTitle']
+	socialDescription?: BaseProps['socialDescription']
+	socialImage?: BaseProps['socialImage']
+	socialUrl?: BaseProps['socialUrl']
+	socialType?: BaseProps['socialType']
+	socialTwitterCard?: BaseProps['socialTwitterCard']
 }
 
 export const site: Site = {
+	lang: 'fr',
 	title: 'Fichiers 2GETHER',
 	author: 'Matiboux',
 	themeColor: '#ffffff',
 	favicon: '/favicon.png',
-	lang: 'fr',
+	viewportScale: 1,
+	socialTitle: true,
+	socialDescription: true,
 }
