6c6bbfb04b59b4f847fa4e17ae0b0112cfada332
Mati <matiboux@gmail.com>
2025-03-02 13:07:56 +0100

    Update packages & build config (#13)
    * Update vscode config
    
    * Move app files
    
    * Update packages & build config
    
    * Update workflow files
    
    * Refactor workflow files
    
    * Use bitnami nginx image in prod
    
    * Update build config & workflows
    
    * Refactor page styles
    
    * Update site config & layout
    
    * Add i18n components
    
    * Fix description meta in base layout file
    
    * Add i18n config
    
    * Update tailwind css and integration
    
    * Install build dependencies
    
    * Remove unused tailwind plugin
    
    * Update Dockerfile
    
    * Rename style files
    
    * Update packages
    
    * Fix tailwind reference in style block
    
    * Remove sass
    
    * Update layouts & site config
    
    * Update layouts
    
    * Update astro config
    
    * Refactor build config
    
    * Update packages

---
+++ app/app/astro.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig, envField } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+
+import { i18n } from '/src/config'
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [],
+	site: 'https://files.2gether-asso.fr',
+	vite: {
+		plugins: [
+			tailwindcss(),
+		],
+	},
+	i18n: i18n,
+	env: {
+		schema: {
+			GITHUB_REPOSITORY_URL: envField.string({ context: 'client', access: 'public', optional: true }),
+			GITHUB_SHA: envField.string({ context: 'client', access: 'public', optional: true }),
+		},
+		validateSecrets: true,
+	},
+})
---
+++ app/app/src/site.ts
@@ -0,0 +1,31 @@
+import type { Props as BaseProps } from '~/layouts/Base.astro'
+
+export interface Site
+{
+	lang?: BaseProps['lang']
+	title?: BaseProps['title']
+	description?: BaseProps['description']
+	author?: BaseProps['author']
+	keywords?: BaseProps['keywords']
+	generator?: BaseProps['generator']
+	themeColor?: BaseProps['themeColor']
+	viewportScale?: BaseProps['viewportScale']
+	favicon?: BaseProps['favicon']
+	socialTitle?: BaseProps['socialTitle']
+	socialDescription?: BaseProps['socialDescription']
+	socialImage?: BaseProps['socialImage']
+	socialUrl?: BaseProps['socialUrl']
+	socialType?: BaseProps['socialType']
+	socialTwitterCard?: BaseProps['socialTwitterCard']
+}
+
+export const site: Site = {
+	lang: 'fr',
+	title: 'Fichiers 2GETHER',
+	author: 'Matiboux',
+	themeColor: '#ffffff',
+	favicon: '/favicon.png',
+	viewportScale: 1,
+	socialTitle: true,
+	socialDescription: true,
+}
